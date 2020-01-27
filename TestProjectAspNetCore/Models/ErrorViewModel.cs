caf6136833c69796be81cc62b1c8f700ec0faafd
Raj K <Raj.Konatham@integrisok.com>
2020-01-27 12:13:44 -0500

    First commit

---
+++ TestProjectAspNetCore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TestProjectAspNetCore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
